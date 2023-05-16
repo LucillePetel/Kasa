9d07c482da4f397355f89753a361caedabef6754
lucille Petel <lucille.petel@gmail.com>
2023-05-16 17:30:13 +0200

    Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./Pages/About";
+import Annonce from "./Pages/Annonce";
+import Error from "./Pages/Error";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="kasa">Kasa</header>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/annonce" exact element={<Annonce />} />
+          <Route path="/About" exact element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../components/Footer";
+
+const Error = () => {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Error;
